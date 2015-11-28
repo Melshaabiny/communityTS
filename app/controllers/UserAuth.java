7d79b56ef37cd66fa441862013972f461ec42fa2
Moustafa Elshaabiny <moustafa.elshaabiny@gmail.com>
2015-11-28 00:57:29 -0500

    added profile view, action, and routes

--- app/controllers/UserAuth.java
+++ app/controllers/UserAuth.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import javafx.beans.property.ReadOnlyStringProperty;
 import models.User;
 import play.*;
 import play.data.DynamicForm;
@@ -9,7 +10,7 @@ import play.mvc.*;
 import views.html.*;
 
 /**
- * Created by MoustafaElshaabiny on 11/24/15.
+ * Created by MoustafaElshaabiny
  */
 
 public class UserAuth extends Controller {
@@ -18,29 +19,40 @@ public class UserAuth extends Controller {
         return ok(views.html.userauth.index.render());
     }
 
+    public Result profile(String usrname){
+        User user = User.find.where().eq("username", usrname).findUnique();
+        return ok(views.html.userauth.profile.render(user));
+    }
+
     public Result login() {
+        // Get username and password from the login form
         DynamicForm userForm = form().bindFromRequest();
         String username = userForm.data().get("username");
         String password = userForm.data().get("password");
 
+        // Find the user's information in the database
         User user = User.find.where().eq("username", username).findUnique();
 
         if (user != null && user.authenticate(password)) {
             session("user_id", user.id.toString());
             flash("success", "Welcome back " + user.username);
-        } else {
+        }
+        else {
             flash("error", "Invalid login. Check your username and password.");
         }
 
         return redirect(routes.Application.index());
     }
-    //@Security.Authenticated(SecAuth.class)
+
     public Result signup() {
+        // Get username and password from the signup form
         DynamicForm userForm = form().bindFromRequest();
+        String name = userForm.data().get("name");
         String username = userForm.data().get("username");
         String password = userForm.data().get("password");
 
-        User user = User.createUser(username, password);
+        // Create a new user with the username and password obtained above
+        User user = User.createUser(name, username, password);
 
         if (user == null) {
             flash("error", "Invalid user");
@@ -49,8 +61,8 @@ public class UserAuth extends Controller {
 
         user.save();
 
-        flash("success", "Welcome new user " + user.username);
-        session("user_id", user.id.toString());
+//        flash("success", "Welcome new user " + user.username);
+//        session("user_id", user.id.toString());
         return redirect(routes.Application.index());
     }
 }
